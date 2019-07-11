8065f3ea623201f218166b64a254c6a9dcf4b957
chayapol <iamchayapol@gmail.com>
2019-07-11 10:42:27 +0700

    test

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -40,6 +40,7 @@ class _RegisterState extends State<Register> {
       myAlert(response.code, response.message);
     });
   }
+  
 
   void myAlert(String titleStr, String messageStr) {
     showDialog(
